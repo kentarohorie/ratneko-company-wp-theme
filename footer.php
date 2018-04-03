c0124490d3144de090101dda13d35766ae4de844
kentarohorie <kentaro.horie.4@gmail.com>
2018-04-03 09:47:14 +0900

    Update

--- footer.php
+++ footer.php
@@ -2,14 +2,14 @@
   <div class="container">
     <div class="row">
       <div class="col l6 s12">
-        <h5 class="white-text">Ratneko Inc.</h5>
+        <h5>Ratneko Inc.</h5>
         <p class="grey-text text-lighten-4"></p>
       </div>
       <div class="col l4 offset-l2 s12">
         <ul>
-          <li><a class="grey-text text-lighten-3" href="<?php echo home_url('company'); ?>">会社概要</a></li>
-          <li><a class="grey-text text-lighten-3" href="<?php echo home_url('business'); ?>">事業内容</a></li>
-          <li><a class="grey-text text-lighten-3" href="<?php echo home_url('inquiry'); ?>">お問い合わせ</a></li>
+          <li><a class="" href="<?php echo home_url('company'); ?>">会社概要</a></li>
+          <li><a class="" href="<?php echo home_url('business'); ?>">事業内容</a></li>
+          <li><a class="" href="<?php echo home_url('inquiry'); ?>">お問い合わせ</a></li>
         </ul>
       </div>
     </div>
--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,6 @@
-<ul id="slide-out" class="sidenav">
+<ul id="slide-out" class="sidenav collection">
   <li><h3>Ratneko Inc.</h3></li>
-  <li><a class="" href="<?php echo home_url('company'); ?>">会社概要</a></li>
-  <li><a class="" href="<?php echo home_url('business'); ?>">事業内容</a></li>
-  <li><a class="" href="<?php echo home_url('inquiry'); ?>">お問い合わせ</a></li>
+  <li class="collection-item"><a class="" href="<?php echo home_url('company'); ?>">会社概要</a></li>
+  <li class="collection-item"><a class="" href="<?php echo home_url('business'); ?>">事業内容</a></li>
+  <li class="collection-item"><a class="" href="<?php echo home_url('inquiry'); ?>">お問い合わせ</a></li>
 </ul>
